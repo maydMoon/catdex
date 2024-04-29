6d09183d157317786f14d771238c0777ee79494a
Maylla Krislainy Matos de Sousa <47458443+maydMoon@users.noreply.github.com>
2024-04-29 14:55:16 -0300

    Header + List Components
    - Ajustes das configurações scss

--- src/app/modules/catdex/pages/home/home.component.ts
+++ src/app/modules/catdex/pages/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 
 // Components
 import { HeaderComponent } from '../../components/header/header.component';
+import { ListComponent } from '../../components/list/list.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, ListComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
